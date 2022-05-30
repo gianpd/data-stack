fcb33355e0d8832d6c0ff6470171b1bf9115794c
gianpd <gpdomiziani@gmail.com>
2022-05-30 12:31:03 +0200

    data-ingest pipe implemented. Events generator added.

--- project/data-ingest/app/core_api.py
+++ project/data-ingest/app/core_api.py
@@ -1,19 +1,15 @@
-from http.client import responses
 import os
 import sys
+import json
 import pathlib
 import logging
 logging.basicConfig(stream=sys.stdout, format='%(asctime)-15s %(message)s',
                 level=logging.INFO, datefmt=None)
 logger = logging.getLogger("data-ingestion")
 
-from fastapi import APIRouter, HTTPException, File, UploadFile
-
 from io import BytesIO
-
+from fastapi import APIRouter, HTTPException, File, UploadFile, Request
 import pandas as pd
-
-# from app.models.pydantic import DataIngest
 
 from app.pipeline import DataIngestPipeline
 
@@ -22,16 +18,38 @@ pipe = DataIngestPipeline()
 router = APIRouter()
 
 @router.post("/", response_model=None, status_code=201)
-async def ingest_data(file: UploadFile = File(...)) -> None:
-    logger.info('Data ingest POST received')
-    fname = file.filename
-    if not '.csv' in fname:
-        logger.error('data must be in csv format.')
-        raise HTTPException(status_code=401, detail=f'data must be in csv format: {fname}')
-    contents = await file.read()
-    buffer = BytesIO(contents)
-    response_error = pipe.ingest(buffer)
-    if response_error:
-        raise HTTPException(status_code=406, detail=response_error['detail'])
+async def ingest_data(event: Request):
+    """
+    CORE_API POST ingest method: exposed POST method for receiving events raw data.
+    """
+    try:
+        event_dict = await event.json()
+        logger.info(f'Received event: {event_dict}')
+    except json.JSONDecodeError as e:
+        logger.error(e)
+        raise HTTPException(status_code=500, detail=str(e))
+
+    ### check if event contains the wanted fields
+    error_field = pipe.event_check(event_dict)
+    if error_field:
+        raise HTTPException(status_code=406, detail=f"{error_field} must be present in event.")
+    pipe.ingest_raw(event_dict)
+    logger.info(f'POST/ingest/ precessed correctly.')
+
+
+
+# @router.post("/", response_model=None, status_code=201)
+# async def ingest_data(file: UploadFile = File(...)) -> None:
+#     logger.info('Data ingest POST received')
+#     fname = file.filename
+#     if not '.csv' in fname:
+#         logger.error('data must be in csv format.')
+#         raise HTTPException(status_code=401, detail=f'data must be in csv format: {fname}')
+#     contents = await file.read()
+#     buffer = BytesIO(contents)
+#     response_error = pipe.ingest(buffer)
+#     if response_error:
+#         raise HTTPException(status_code=406, detail=response_error['detail'])
+
     
     
--- project/data-ingest/app/pipeline.py
+++ project/data-ingest/app/pipeline.py
@@ -1,7 +1,10 @@
 import os
 import sys
 import pathlib
+import json
 import logging
+
+from requests import get
 logging.basicConfig(stream=sys.stdout, format='%(asctime)-15s %(message)s',
                 level=logging.INFO, datefmt=None)
 logger = logging.getLogger("data-ingestion-pipeline")
@@ -11,17 +14,63 @@ from dataclasses import dataclass
 from io import BytesIO
 import pandas as pd
 
+from datetime import datetime
+
+import shutil
+
 
 from typing import List, Dict, Optional
+
+MUST_HAVE_FIELDS = [
+    "client.user_id", # the ID of the user that did the upload or the download
+    "direction", 
+    "timestamp", # when the event was sent to the server
+    "size", # the total size of the uploaded/downloaded payload, in bytes
+    "time.backend", # the time needed to transfer the payload, in milliseconds
+    "status", # the operation’s result - [success, fail]
+]
+
+def get_suffix():
+    n = datetime.now()
+    return datetime.strftime(n, '%Y%m%d_%s')
 
 
 @dataclass
 class DataIngestPipeline:
-    def ingest(self, buffer: BytesIO) -> Dict:
+    def ingest_raw(self, event: Dict) -> Dict:
+        """
+        It allows to save the received event to the disk, as received by the server (without perform any preprocessing).
+
+        --Parameters:
+         - event: Dict, the event as received by the server.
+
+        return -> bool 
+        """
+        suffix = get_suffix()
         try:
-            df = pd.read_csv(buffer)
-            logger.info(df.head(2))
-            return None
-        except pd.errors.EmptyDataError as e:
-            logger.error(e)
-            return dict(detail=str(e))
+            with open(f'shared/event_{suffix}.json', '+w') as f:
+                json.dump(event, f)
+        except:
+            raise DataIngestException(f" Error in DataIngestPipeline: {sys.exc_info()[0]} {sys.exc_info()[1]}") 
+
+    def event_check(self, event: Dict) -> bool:
+        """
+        Event checker: checks if some crucial fields are present in the received event.
+
+        --Parameters:
+         - event: Dict, the event as received by the server.
+
+        return -> Error: str (i.e the not-present field) / SUCCESS: None
+        """
+        keys = list(event.keys())
+        for field in MUST_HAVE_FIELDS:
+            if field not in keys:
+                logger.error(field, 'not in keys')
+                return field
+        logger.info('Data ingest> event check passed.')
+        return None
+
+
+
+class DataIngestException(Exception):
+    pass
