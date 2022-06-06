d257ed9a8cfde0db071f93aba20dab2f20e36ef7
gianpd <gpdomiziani@gmail.com>
2022-06-06 11:48:48 +0200

    minor

--- project/data-ingest/bootstrap.sh
+++ project/data-ingest/bootstrap.sh
@@ -8,6 +8,7 @@ if [ $APPLICATION == "zip-events" ]; then
   cd app
   python asynch_launcher.py apply-daily-lifecycle
 fi
+
 if [ $APPLICATION == "start-server" ]; then
   uvicorn app.main:app --reload --workers 1 --host 0.0.0.0 --port 5010
 fi
